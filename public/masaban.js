5e8d9870e02417f43e12612b71a95c7f636d1d70
Simon Kendall <simon.a.kendall@gmail.com>
2024-05-31 14:35:55 +0200

    add eslint
    Co-authored-by: Ludvig Fendert <ludvig.fendert@appliedtechnology.se>
    Co-authored-by: Golda <goldamarymanackan@gmail.com>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>

--- public/masaban.js
+++ public/masaban.js
@@ -12,7 +12,11 @@ function grab_card(evt, elem) {
   grabbedCard = elem;
   console.log(evt);
   grabbedCard.classList.add("grabbed");
-  if(grabbedCard.dataset.initialClickLocationX) {
+  if (grabbedCard.dataset.initialClickLocationX) {
+    /*console.log("evt clientX is " + evt.clientX);
+    console.log("initialClickLocation is " + grabbedCard.dataset.initialClickLocationX);
+
+    grabbedCard.dataset.initialClickLocationX += Number(evt.clientX - grabbedCard.dataset.initialClickLocationX);*/
     return;
   }
   grabbedCard.dataset.initialClickLocationX = evt.clientX;
@@ -20,23 +24,24 @@ function grab_card(evt, elem) {
 }
 
 function mouse_move(event) {
-  if(grabbedCard) {
-    if(!gotListener) {
+  if (grabbedCard) {
+    if (!gotListener) {
       document.addEventListener("click", drop_card);
       gotListener = true;
     }
-    const translateX = event.clientX - grabbedCard.dataset.initialClickLocationX;
-    const translateY = event.clientY - grabbedCard.dataset.initialClickLocationY;
+    const translateX =
+      event.clientX - grabbedCard.dataset.initialClickLocationX;
+    const translateY =
+      event.clientY - grabbedCard.dataset.initialClickLocationY;
 
     let str = translateX + "px " + translateY + "px";
     console.log(str);
     grabbedCard.style.translate = str;
   }
-
 }
 
 document.querySelectorAll(".grid-item").forEach((elem) => {
-  elem.addEventListener("click", function(event) {
+  elem.addEventListener("click", function (event) {
     grab_card(event, elem);
   });
 });
