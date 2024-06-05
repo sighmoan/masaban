74b7697fe49b76f0e5dbfd156b0fed8c6e3fcbc9
Simon Kendall <simon.a.kendall@gmail.com>
2024-06-05 18:35:41 +0200

    add java backend

---
+++ src/main/java/com/masagal/masaban_server/controllers/MasabanController.java
@@ -0,0 +1,28 @@
+package com.masagal.masaban_server.controllers;
+
+import com.masagal.masaban_server.model.Board;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1")
+public class MasabanController {
+
+    Board board;
+
+    public MasabanController(Board board) {
+        this.board = board;
+    }
+
+    @PostMapping("/board/")
+    public ResponseEntity<String> storeBoard(@RequestBody Board board) {
+        this.board = board;
+        return ResponseEntity.ok("yep");
+    }
+
+    @GetMapping("/board/")
+    public Board getBoard() {
+        return board;
+    }
+}
